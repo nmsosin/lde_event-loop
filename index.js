45f24b4e811ea7f420758b35f6ea9ec9a89ed29d
Nikolay Sosin <nmsosin@yandex.ru>
2023-10-25 17:00:45 +0300

    Minor fix

--- index.js
+++ index.js
@@ -9,25 +9,36 @@
 //   1. микрозадача
 //   2. Рендер задача (например изменение содержание элемента)
 
+const tasks = document.querySelectorAll(".page__checkbox");
+const indicator = document.querySelector(".page__indicator");
+let isTaskDone = false;
+let isFinished = false;
+
 function createQueues () {
   // Task 1
-  console.log('Task 1')
   setTimeout(() => {
-    // Microtask 1.1
+    console.log('Task 1')
     Promise.resolve()
-    .then(() => console.log('Microtask 1.1'))
-    // Render task 1.2
-    console.log('Render task 1.2')
-    const tasks = document.querySelectorAll(".page__checkbox");
-    for (const task of tasks) {
-      task.classList.add('page__checkbox_done')
-    }
+    .then(() => {
+      // Microtask 1.1
+      console.log('Microtask 1.1')
+      isTaskDone = true;
+
+      // Render task 1.2
+      console.log('Render task 1.2')
+      
+      for (const task of tasks) {
+        if (isTaskDone) {
+          task.classList.add('page__checkbox_done')
+        }
+      }
+  })
 
   }, 1000)
 
   // Task 2
-  console.log('Task 2')
   setTimeout(() => {
+    console.log('Task 2')
     // Microtask 2.1
     Promise.resolve()
     .then(() => console.log('Microtask 2.1'))
@@ -38,14 +49,20 @@ function createQueues () {
   }, 2000)
 
   // Task 3
-  console.log('Task 3')
   setTimeout(() => {
+    console.log('Task 3')
     // Microtask 3.1
     Promise.resolve()
-    .then(() => console.log('Microtask 3.1'))
-    // Render task 3.2
-    console.log('Render task 3.2')
-    document.querySelector(".page__indicator").classList.add('page__indicator_done')
+    .then(() => {
+      console.log('Microtask 3.1')
+      isFinished = true;
+
+      // Render task 3.2
+      console.log('Render task 3.2')
+      if (isFinished) {
+        indicator.classList.add('page__indicator_done')
+      }
+    })
   }, 3000)
 }
 document.querySelector(".page__button").addEventListener('click', (evt) => {
@@ -54,15 +71,15 @@ document.querySelector(".page__button").addEventListener('click', (evt) => {
 })
 
 //   Дополнительное задание:
-// • Реализовать клон функции map(отдельная функция или прототип для массивов на ваше усмотрение)
+// • Реализовать клон функции map (отдельная функция или прототип для массивов на ваше усмотрение)
 
 const customMap = (array, cb) => {
-  const tempArr = [];
+  const result = [];
   for (let i = 0; i < array.length; i++) {
-    tempArr.push(cb(array[i]))
+    result.push(cb(array[i]))
   }
 
-  return tempArr;
+  return result;
 }
 
 const testArray = [1, 2, 3, 4, 5, 6];
@@ -70,8 +87,9 @@ const double = (x) => x * 2;
 const objectize = (a) => {
   const obj = {}
   obj[a] = a
+
   return obj
 }
 
-console.log(customMap(testArray, double));
-console.log(testArray, objectize);
+console.log(customMap(testArray, double)); // => expect: [2, 4, 6, 8, 10, 12]
+console.log(customMap(testArray, objectize)); // => expect: [{1: 1}, {2: 2}, {3: 3}, {4: 4}, {5: 5}, {6: 6}]
